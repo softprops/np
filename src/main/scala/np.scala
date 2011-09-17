7172d857028c26a1e163b5136418711f62ab0ee7
softprops <d.tangren@gmail.com>
2011-09-17 14:43:13 -0400

    move everything into np inner obj

--- src/main/scala/np.scala
+++ src/main/scala/np.scala
@@ -1,56 +1,54 @@
 import sbt._
 import Project.Initialize
 
-
-object BuildSbt {
-
-  private val SbtVersion = "{sbtVersion}"
-
-  /** Handles cross sbt versioning.  */
-  private def versionBind(version: String, plugin: Boolean) = {
-    if(!plugin) """:= "%s"""".format(version)
-    else {
-      if(version.contains(SbtVersion)) {
-        """<<= sbtVersion(v => "%s".format(v))""".format(version.replace(SbtVersion, "%s"))
-      } else """:= "%s"""".format(version)
-    }
-  }
-  def apply(plugin: Boolean, org: String, name: String, version: String) =
-    """%sorganization := "%s"
-    |
-    |name := "%s"
-    |
-    |version %s""".stripMargin.format(
-      if(plugin) "sbtPlugin := true\n\n" else "",
-      org,
-      name,
-      versionBind(version, plugin)
-    )
-}
-
-private [this] object Usage {
-  val display = """
-  |Usage: <key>:<value> <key2>:<value2> ...
-  |
-  |all key arguments have default values
-  |
-  |Keys
-  |  org     Project organization. Defaults to sbt built-in default
-  |  name    Project name. Defaults to sbt built-in default
-  |  version Project version. Defaults to sbt built-in default
-  |  plugin  Boolean indicator of whether the project is a plugin project. Defaults to false
-  |  dir     Path to dir where np should generate project. Defaults to '.'
-  |""".stripMargin
-}
-
 object Plugin extends sbt.Plugin {
-  import Keys._
   import sbt.Keys._
 
   object np {
 
     import java.io.File
     import java.lang.Boolean.{parseBoolean => bool}
+
+    private [np] object BuildSbt {
+      private val SbtVersion = "{sbtVersion}"
+
+      /** Handles cross sbt versioning.  */
+      private def versionBind(version: String, plugin: Boolean) = {
+        if(!plugin) """:= "%s"""".format(version)
+        else {
+          if(version.contains(SbtVersion)) {
+            """<<= sbtVersion(v => "%s".format(v))""".format(version.replace(SbtVersion, "%s"))
+          } else """:= "%s"""".format(version)
+        }
+      }
+
+      def apply(plugin: Boolean, org: String, name: String, version: String) =
+        """%sorganization := "%s"
+        |
+        |name := "%s"
+        |
+        |version %s""".stripMargin.format(
+          if(plugin) "sbtPlugin := true\n\n" else "",
+          org,
+          name,
+          versionBind(version, plugin)
+        )
+    }
+
+    private [np] object Usage {
+      val display = """
+      |Usage: <key>:<value> <key2>:<value2> ...
+      |
+      |all key arguments have default values
+      |
+      |Keys
+      |  org     Project organization. Defaults to sbt built-in default
+      |  name    Project name. Defaults to sbt built-in default
+      |  version Project version. Defaults to sbt built-in default
+      |  plugin  Boolean indicator of whether the project is a plugin project. Defaults to false
+      |  dir     Path to dir where np should generate project. Defaults to '.'
+      |""".stripMargin
+    }
 
     case class Defaults(org: String, name: String, version: String,
                     plugin: Boolean = false, dir: String = ".")
@@ -105,44 +103,44 @@ object Plugin extends sbt.Plugin {
           out.log.info(Usage.display)
      }
 
-  // will auto-mix into project settings
-  def settings: Seq[Setting[_]] = inConfig(Config)(Seq(
-    defaults <<= (name, organization, version)(Defaults(_, _, _)),
-    usage <<= usageTask,
-    check <<= inputTask { (argsTask: TaskKey[Seq[String]]) =>
-      (argsTask, baseDirectory, streams, defaults in Config) map {
-        (args, bd, out, defaults) =>
-          val (_, base) = extract(args, bd, defaults)
-          val (bf, dirs) = (new File(base, "build.sbt"), genDirs(base))
-          dirs :+ bf filter(_.exists) match {
-            case Nil =>
-              out.log.info("Looks good. Run `np` task to generate project.")
-            case existing =>
-              out.log.warn("The following files/directories already exist\n\n%s".format(
-                existing.mkString("\n\n"))
-              )
-          }
+    // will NOT auto-mix into project settings
+    def settings: Seq[Setting[_]] = inConfig(Config)(Seq(
+      defaults <<= (name, organization, version)(Defaults(_, _, _)),
+      usage <<= usageTask,
+      check <<= inputTask { (argsTask: TaskKey[Seq[String]]) =>
+        (argsTask, baseDirectory, streams, defaults in Config) map {
+          (args, bd, out, defaults) =>
+            val (_, base) = extract(args, bd, defaults)
+            val (bf, dirs) = (new File(base, "build.sbt"), genDirs(base))
+            dirs :+ bf filter(_.exists) match {
+              case Nil =>
+                out.log.info("Looks good. Run `np` task to generate project.")
+              case existing =>
+                out.log.warn("The following files/directories already exist\n\n%s".format(
+                  existing.mkString("\n\n"))
+                )
+            }
+        }
       }
-    }
-  )) ++ Seq(
-    Keys.np <<= inputTask { (argsTask: TaskKey[Seq[String]]) =>
-      (argsTask, baseDirectory, streams, defaults in Config) map {
-        (args, bd, out, defaults) =>
-          val (scpt, base) = extract(args, bd, defaults)
-          val (bf, dirs) = (new File(base, "build.sbt"), genDirs(base))
+    )) ++ Seq(
+      Keys.np <<= inputTask { (argsTask: TaskKey[Seq[String]]) =>
+        (argsTask, baseDirectory, streams, defaults in Config) map {
+          (args, bd, out, defaults) =>
+            val (scpt, base) = extract(args, bd, defaults)
+            val (bf, dirs) = (new File(base, "build.sbt"), genDirs(base))
 
-          // error out if any of the target files to generate
-          // already exist
-          if((dirs :+ bf).find(_.exists).isDefined) error(
-            "\nexisting project detected at the path %s" format bf.getParent
-          )
+            // error out if any of the target files to generate
+            // already exist
+            if((dirs :+ bf).find(_.exists).isDefined) error(
+              "\nexisting project detected at the path %s" format bf.getParent
+            )
 
-          IO.write(bf, scpt)
-          out.log.info("Generated build file")
+            IO.write(bf, scpt)
+            out.log.info("Generated build file")
 
-          IO.createDirectories(dirs)
-          out.log.info("Generated source directories")
-       }
-    })
-  }
+            IO.createDirectories(dirs)
+            out.log.info("Generated source directories")
+        }
+     })
+   }
 }
